4617f356b286a966ae9a285b0c0d205b4fbc4321
santiago.sabalain <ssabalain@gmail.com>
2022-06-24 19:11:52 +0200

    Updating python functions

--- facial_database/python_scripts/initial_database_setup.py
+++ facial_database/python_scripts/initial_database_setup.py
@@ -41,20 +41,23 @@ def drop_all_tables():
     for datasets in imdb_datasets['datasets']:
         db.drop_table(sql_user,sql_pwd,db_name,datasets['name'])
 
-def main():
+def create_tables():
+
+    imdb_datasets = json.load(open(root_facialdb_folder + imdb_metadata_folder + imdb_metadata_file))
+
+     #Creating tables in SQL and loading the data from the .tsv files
+    for datasets in imdb_datasets['datasets']:
+        table_name = datasets['name']
+        columns = datasets['columns']
+        db.create_table(sql_user,sql_pwd,db_name,table_name,columns)
+
+def load_data():
 
     conn = db.return_conn(uri) #sqlAlchemy version
     #conn,cursor = db.connect_to_mysql(sql_user,sql_pwd,db_name)
 
-    download_datasets()
-
-    #If dropping is enabled, we will first drop the existing tables
-    if dropping == 1:
-        drop_all_tables()
-
     imdb_datasets = json.load(open(root_facialdb_folder + imdb_metadata_folder + imdb_metadata_file))
 
-    #Creating tables in SQL and loading the data from the .tsv files
     for datasets in imdb_datasets['datasets']:
         table_name = datasets['name']
         columns = datasets['columns']
@@ -66,28 +69,25 @@ def main():
             dtypes[key] = columns['py_data_type']
 
         columns = datasets['columns']
-        db.create_table(sql_user,sql_pwd,db_name,table_name,columns)
-        
+
         #Loading the data all at once is not possible due to RAM overloading. In order to accomplish the upload,
-        #   we'll perform a chunck load.
+        # we'll perform a chunck load.
         #Chunk size can be modified according to available resources.
         chunk_size=100000
-        batch_no=0
-
         file_path = root_facialdb_folder + datasets_folder + file_name
 
-        starttime = time.time()
-        for chunk in pd.read_csv(file_path,compression='gzip', sep= '\t',dtype= dtypes,chunksize=chunk_size,iterator=True):
-            chunk.to_sql(con = conn, name = table_name, if_exists = 'append',index= False) 
-            #Can be done using sqlAlchemy, but it's slower:
-            #db.insert_df_in_table(conn,cursor,chunk,table_name)
-            batch_no+=1
+        db.insert_df_in_table(conn,file_path,dtypes,chunk_size,table_name)
 
-            if ((batch_no % 25) == 0 or batch_no == 1):
-                print("Inserting records from batch: " + str(batch_no) + " of table " + table_name + ". Batch size: " + str(chunk_size))
-        
-        endtime = time.time()
-        print("All records for table " + table_name + " were inserted. Overall execution time (secs): " + str(endtime - starttime))
+def main():
+
+    download_datasets()
+
+    #If dropping is enabled, we will first drop the existing tables
+    if dropping == 1:
+        drop_all_tables()
+
+    create_tables()
+    load_data()
 
 # if __name__ == '__main__':
 #     main()
